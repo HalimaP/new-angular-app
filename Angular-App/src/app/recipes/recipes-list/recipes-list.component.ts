4f63bd994149fe16ca1ce2134f8e7ccc86252a3d
Halima Potur <halima.potur@compusight.com>
2021-03-24 11:47:00 +0100

    Working on recipe-detail and recipes-list components

--- Angular-App/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Angular-App/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+  new Recipe('A Test Recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+];
+
 
   constructor() { }
 
   ngOnInit(): void {
+ 
+   this.max();
+   
   }
+max(): void{
+  let arr =[1050,7,5,9, 12, 50,150, 2050];
+  let curr_max_numb = arr[0];
+  let min ;
+  for(let i = 0; i <=arr.length; i++){
+ 
+    if (curr_max_numb < arr[i]){
+      // console.log(this.arr[i]);
+      curr_max_numb = arr[i];
+      
+      }
+     
+} 
+console.log("CurrMax::  ",curr_max_numb);
+for(let i =0; i < arr.length; i++){
 
+}
+console.log("Min number is::  ",min);
+
+}
 }
