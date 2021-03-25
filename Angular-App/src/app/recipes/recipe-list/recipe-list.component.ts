35401a35ee92ec4ddbb7a59698f24658bdb13a31
Halima Potur <halima.potur@compusight.com>
2021-03-25 12:00:36 +0100

    Adding method min and max

--- Angular-App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,7 +15,32 @@ export class RecipeListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-}
+  ngOnInit(): void {
+ 
+    this.max();
+    
+   }
+ max(): void{
+   let arr =[1050,7,5,9, 12, 50,150, 2050, 1, 50100];
+   let curr_max_numb = arr[0];
+   let min = arr[0];
+   for(let i = 0; i <=arr.length; i++){
+  
+     if (curr_max_numb < arr[i]){
+       // console.log(this.arr[i]);
+       curr_max_numb = arr[i];
+       
+       }
+      
+ } 
+ console.log("CurrMax::  ",curr_max_numb);
+ for(let i =0; i <= arr.length; i++){
+ if (min > arr[i]){
+   min = arr[i];
+ }
+ }
+ console.log("Min number is::  ",min);
+ 
+ }
+ }
+ 
